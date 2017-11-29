41a0c5ed7ca440f7a89d1e192d39a658cba58483
liyinuo0823 <you@example.compip>
2017-11-29 21:09:16 +0800

    add new file

---
+++ 11-28.py
@@ -0,0 +1,28 @@
+"""
+这是我的作业
+
+"""
+#算数运算符
+a=21
+b=10
+c=0
+c=a+b
+print("Line1-Value of c is" ,c)
+c=a-b
+print ("Line2-Value of c is" ,c)
+c=a*b
+print("Line3-Value of c is",c)
+c=a/b
+print("Line4-Value of c is",c)
+c=a%b
+print ("Line5-Value of c is",c)
+a=2
+b=3
+c=a**b
+print("Line6-Value of c is",c)
+a=11
+b=5
+c=a//b
+print("Line7-Value of c is",c)
+#比较运算符
+print(2!=9)
